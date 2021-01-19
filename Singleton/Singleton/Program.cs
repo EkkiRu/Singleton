7f2bc4108641fd84f1f4cef49d2cb983208f7f7b
EkkiRu <63056889+EkkiRu@users.noreply.github.com>
2021-01-19 17:10:42 +0600

    Homework
    Homewrok

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.CreateOrGetInstance();
+            singleton.ToString();
+            singleton = Singleton.CreateOrGetInstance();
+            singleton.ToString();
+            singleton.ToString();
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class Singleton
+    {
+        /*1. Создать класс синглтон, 
+в этом классе будет храниться объект List<int> LST с 100тыс рандомных значений в диапазоне от -1000 то 1000
+2. Создать 2 обращения к этому классу 
+3. Вывести сумму и количество всех положительных значений из коллекции LST. Если это синглтон, 
+        эти значения должны совпадать у разных переменных, т.к. они будут смотреть на один и тот же объект*/
+        static Singleton Instance;
+        static List<int> LST = new List<int>();
+        public static Singleton CreateOrGetInstance()
+        {
+            if (Instance == null)
+                Instance = new Singleton();
+            return Instance;
+        }
+
+        private Singleton()
+        {
+        }
+        public override string ToString()
+        {
+            Random random = new Random();
+            for(int i = 0; i < 10; i++)
+            {
+                LST.Add(random.Next());
+                Console.WriteLine(LST[i].ToString());
+            }
+            return "";
+        }
+    }
+}
